7fac96f0f7ba9fc0ef82412d3e459a9378e08bbf
harry.sjt <harry.sjt@alibaba-inc.com>
2018-07-05 21:54:15 +0800

    Design Pattern/Visitor

---
+++ DesignPatterns/VisitorPattern/src/VisitorPattern.java
@@ -0,0 +1,86 @@
+public class VisitorPattern {
+    /* Visitor Pattern
+     * Q1 What is visitor pattern
+     * >>> to separate an algorithm from an object on which it operates
+     *
+     * Q2 Benefits
+     * >>> add new operations without modifying object structure
+     * >>> when new operations are needed frequently and that object consists of many unrelated
+     * >>> classes, to distribute these operations among all these classes is not good
+     *
+     * Q3 Essential
+     * >>> using function overloading to conduct different behaviours
+     *
+     * Q4 Attention
+     * >>> accept & visit should better use "final" parameters, as they only traverse the structure
+     * >>> rather than changing them
+     * */
+
+    public static void main(String[] args) {
+        Movie m1 = new ScienceFictionMovie("John Carney");
+        m1.accept(new PrintMovieVisitor());
+        m1.accept(new DefaultMovieVisitor());
+
+        Movie m2 = new ComedyMovie("Amy Adams");
+        m2.accept(new PrintMovieVisitor());
+        m2.accept(new DefaultMovieVisitor());
+    }
+}
+
+// base class & its descendants
+interface Movie {
+    void accept(Visitor visitor);
+}
+
+class ScienceFictionMovie implements Movie {
+    String director;
+    public ScienceFictionMovie(String director) {
+        this.director = director;
+    }
+
+    @Override
+    public void accept(final Visitor visitor) {
+        visitor.visit(this);
+    }
+}
+
+class ComedyMovie implements Movie {
+    String star;
+    public ComedyMovie(String star) {
+        this.star = star;
+    }
+
+    @Override
+    public void accept(final Visitor visitor) {
+        visitor.visit(this);
+    }
+}
+
+// base visitor
+interface Visitor {
+    void visit(ScienceFictionMovie scienceFictionMovie);
+    void visit(ComedyMovie comedyMovie);
+}
+
+// different behaviours implemented via different subclass of visitor
+class PrintMovieVisitor implements Visitor {
+    public void visit(final ScienceFictionMovie scienceFictionMovie) {
+        System.out.println("Director: " + scienceFictionMovie.director);
+    }
+
+    public void visit(final ComedyMovie comedyMovie) {
+        System.out.println("Star: " + comedyMovie.star);
+    }
+}
+
+class DefaultMovieVisitor implements Visitor {
+    @Override
+    public void visit(ScienceFictionMovie scienceFictionMovie) {
+        System.out.println("Default director is " + scienceFictionMovie.director);
+    }
+
+    @Override
+    public void visit(ComedyMovie comedyMovie) {
+        System.out.println("Default star is " + comedyMovie.star);
+    }
+}
